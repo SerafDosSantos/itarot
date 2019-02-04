1008f4d2f30045f46f748eeece5d75a7b2a5fc7d
Seraphim Dos Santos <cyb3rsh4t@gmail.com>
2019-02-04 01:49:33 -0500

    fixed image links

--- scripts/tm/all.js
+++ scripts/tm/all.js
@@ -1,13 +1,13 @@
 
 function populateArray(aCrds) {
 	for (var j=1;j<=22;j++) {
-		aCrds.push("../rws/w300px/A"+j+".jpg");
+		aCrds.push("../tm/w300px/A"+j+".jpg");
 	}
 	for (var i=1;i<=14;i++) {
-		aCrds.push("../rws/w300px/S"+i+".jpg");
-		aCrds.push("../rws/w300px/C"+i+".jpg");
-		aCrds.push("../rws/w300px/W"+i+".jpg");
-		aCrds.push("../rws/w300px/D"+i+".jpg");
+		aCrds.push("../tm/w300px/S"+i+".jpg");
+		aCrds.push("../tm/w300px/C"+i+".jpg");
+		aCrds.push("../tm/w300px/W"+i+".jpg");
+		aCrds.push("../tm/w300px/D"+i+".jpg");
 	}
 	return aCrds;
 }
